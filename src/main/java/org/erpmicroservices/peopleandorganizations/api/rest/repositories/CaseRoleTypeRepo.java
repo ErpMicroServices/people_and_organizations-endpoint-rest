2e74a650e746d79e48b1a682c7670e9a4af14230
Jim Barrows <Jim.Barrows@gmail.com>
2022-09-10 00:35:18 -0500

    Added some more standard yml config.  Upgraded spring boot and java, as well as some other jars.

--- src/main/java/org/erpmicroservices/peopleandorganizations/api/rest/repositories/CaseRoleTypeRepo.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/api/rest/repositories/CaseRoleTypeRepo.java
@@ -1,17 +1,18 @@
 package org.erpmicroservices.peopleandorganizations.api.rest.repositories;
 
 import org.erpmicroservices.peopleandorganizations.api.rest.models.CaseRoleType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "caseRoleTypes", path = "caseRoleTypes")
 public interface CaseRoleTypeRepo extends PagingAndSortingRepository<CaseRoleType, UUID> {
 
-	@RestResource(exported = true, path = "/roots", rel = "roots")
-	List<CaseRoleType> findAllByParentIsNull();
 
+	Page<CaseRoleType> findByParentIsNull(Pageable pageable);
+
+	Page<CaseRoleType> findByDescriptionContaining(String description, Pageable pageable);
 }
