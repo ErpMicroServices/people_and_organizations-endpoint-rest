9bbbc7b5ffed1d7c738a1d462fbd9be97169aa0d
Jim Barrows <JIm.Barrows@gmail.com>
2024-02-07 21:59:02 -0600

    7 using the hateoas setup to run off of the repo delete doesnt not handle the accept header correctly (#10)
    * Updated the spring version.  Updated the test library.
    
    * Removed commented out config

---
+++ src/test/java/org/erpmicroservices/peopleandorganizations/service/endpoints/AbstractGwtTestTemplate.java
@@ -0,0 +1,9 @@
+package org.erpmicroservices.peopleandorganizations.service.endpoints;
+
+public abstract class AbstractGwtTestTemplate {
+    public abstract void given();
+
+    public abstract void when();
+
+    public abstract void then();
+}
