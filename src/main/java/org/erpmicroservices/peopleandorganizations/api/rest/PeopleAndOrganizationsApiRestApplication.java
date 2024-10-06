a9b841c4a6b431d3714a450fce1503e931f2b5a7
Jim Barrows <Jim.Barrows@gmail.com>
2024-10-06 17:07:42 -0500

    Setup to have authentication and authorization server up and running

--- src/main/java/org/erpmicroservices/peopleandorganizations/api/rest/PeopleAndOrganizationsApiRestApplication.java
+++ src/main/java/org/erpmicroservices/peopleandorganizations/api/rest/PeopleAndOrganizationsApiRestApplication.java
@@ -6,16 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PeopleAndOrganizationsApiRestApplication {
 
-//    @Autowired
-//    private ObjectMapper objectMapper;
-
     public static void main(String... args) {
         SpringApplication.run(PeopleAndOrganizationsApiRestApplication.class, args);
     }
 
-//    @PostConstruct
-//    public void setUp() {
-//        objectMapper.registerModule(new JavaTimeModule());
-//        objectMapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
-//    }
 }
